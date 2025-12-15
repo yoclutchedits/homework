504ea589c18e218e1eb720bb2f0b33b587148cfa
yoclutchedits <pitambers98@gmail.com>
2025-12-15 19:51:58 +0545

    Refactor Rock, Paper, Scissors game logic

--- homework 4.py
+++ homework 4.py
@@ -1,27 +1,20 @@
 import random
 from colorama import init, Fore
 init(autoreset=True)
-
 print('Welcome to Rock, Paper, Scissors!')
-
 def get_difficulty():
     d = input(f'{Fore.MAGENTA}Choose the difficulty (medium, hard): ').lower()
     if d not in ['medium', 'hard']:
         print('Invalid choice, choose again.')
         return get_difficulty()
-
-
 def rock_paper_scissors():
     choice = ['rock', 'paper', 'scissors']
     d = get_difficulty()
-
     while True:
         c = input(f'{Fore.MAGENTA}What do you choose rock, paper or scissors? ').lower()
-
         if c not in choice:
             print(f'{Fore.RED}Invalid choice. Please choose again.')
             continue
-
         if d == 'medium':
             ai = random.choice(choice)
         else:  # hard
@@ -31,10 +24,8 @@ def rock_paper_scissors():
                 ai = 'scissors'
             else:
                 ai = 'rock'
-
         print(f'You chose: {c}')
         print(f'AI chose: {ai}')
-
         if c == ai:
             print(f"{Fore.YELLOW}It's a tie!")
         elif (
@@ -45,11 +36,9 @@ def rock_paper_scissors():
             print(f'{Fore.GREEN}You win!')
         else:
             print(f'{Fore.RED}AI wins!')
-
         play = input('Do you want to play again? (yes/no): ').lower()
         if play != 'yes':
             print(f'{Fore.GREEN}Thanks for playing!')
             break
-
 if __name__ == "__main__":
     rock_paper_scissors()
